6c634fcd72588fb9b917baf2c14dace0fe212063
Vivek Giri <vivek.giri@algoworks.com>
2019-11-28 18:19:57 +0530

    signIn Page

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _formKey = GlobalKey<FormState>();
   VideoPlayerController playerController;
   var userInf;
   //final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
@@ -61,36 +62,40 @@ class _MyHomePageState extends State<MyHomePage> {
       // and should use the video_player package
     }
   }
+
   GoogleSignIn _googleSignIn = GoogleSignIn(
-  scopes: [
-    'email',
-    'https://www.googleapis.com/auth/contacts.readonly',
-  ],
-);
+    scopes: [
+      'email',
+      'https://www.googleapis.com/auth/contacts.readonly',
+    ],
+  );
+  var passwordVisible = false;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-  
+    passwordVisible = false;
+
     if (kIsWeb) {
       playVideo(
           'https://file-examples.com/wp-content/uploads/2017/04/file_example_MP4_480_1_5MG.mp4');
     }
   }
-Future<void> _handleSignIn() async {
-  try {
-   
-    GoogleSignInAccount g =  await _googleSignIn.signIn();;
-    print(g.email);
-    Scaffold.of(context).showSnackBar(SnackBar(
-      content: Text('Sign  In success with ${g.email}'),
-    ));
-  } catch (error) {
-    Scaffold.of(context).showSnackBar(SnackBar(
-      content: Text('Sign  In Failed'),
-    ));
+
+  Future<void> _handleSignIn() async {
+    try {
+      GoogleSignInAccount g = await _googleSignIn.signIn();
+      print(g.email);
+      Scaffold.of(context).showSnackBar(SnackBar(
+        content: Text('Sign  In success with ${g.email}'),
+      ));
+    } catch (error) {
+      Scaffold.of(context).showSnackBar(SnackBar(
+        content: Text('Sign  In Failed'),
+      ));
+    }
   }
-}
+
   Future<String> signInWithGoogle() async {
     final FirebaseAuth _auth = FirebaseAuth.instance;
     final GoogleSignIn googleSignIn = GoogleSignIn();
@@ -117,47 +122,188 @@ Future<void> _handleSignIn() async {
     return 'signInWithGoogle succeeded: $user';
   }
 
+  var _obsecureText = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text('Webs Render'),
-      ),
       body: kIsWeb
-          ?  
-                Stack(
-                  children: <Widget>[
-                    Positioned(
-                      width:MediaQuery.of(context).size.width*.5,
-                      top:0,
-                      bottom:0,
-                      left:0,
-                      child: Container(
-                        color:Colors.red,
-                        child: Image.asset('assets/images/group2.png',
-                        fit:BoxFit.cover
+          ? Stack(
+              children: <Widget>[
+                Positioned(
+                  width: MediaQuery.of(context).size.width * .5,
+                  top: 0,
+                  bottom: 0,
+                  left: 0,
+                  child: Container(
+                    color: Colors.red,
+                    child: Image.asset('assets/images/group2.png',
+                        fit: BoxFit.cover),
+                  ),
+                ),
+                Positioned(
+                    width: MediaQuery.of(context).size.width * .5,
+                    top: 0,
+                    bottom: 0,
+                    right: 0,
+                    child: Stack(
+                      children: <Widget>[
+                        Positioned(
+                          bottom: 0,
+                          left: 0,
+                          right: 0,
+                          child: Container(
+                            child: Image.asset('assets/images/groupHand.png'),
+                          ),
                         ),
-                      ),
-                    ),
-                    Positioned(
-                      width:MediaQuery.of(context).size.width*.5,
-                      top:0,
-                      bottom:0,
-                      right:0,
-                      child: RaisedButton(
-                        child:Text("Login with googe"),
-                        onPressed: (){
-                          _handleSignIn();
+                        Positioned(
+                          child: Form(
+                            key: _formKey,
+                            child: Container(
+                              padding: EdgeInsets.only(
+                                  left: MediaQuery.of(context).size.width -
+                                      MediaQuery.of(context).size.width * 0.94,
+                                  right: MediaQuery.of(context).size.width -
+                                      MediaQuery.of(context).size.width * 0.94,
+                                  top: 40.0,
+                                  bottom: 10.0),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.max,
+                                children: <Widget>[
+                                  Row(
+                                    mainAxisSize: MainAxisSize.max,
+                                    children: <Widget>[
+                                      Container(
+                                        alignment: Alignment.centerLeft,
+                                        child: Text(
+                                          'Login',
+                                          textScaleFactor: 1.5,
+                                        ),
+                                      ),
+                                      Spacer(),
+                                      Container(
+                                        // color: Colors.red,
 
-                        },
-                      ),
+                                        margin: EdgeInsets.only(left: 0),
+                                        alignment: Alignment.centerRight,
+                                        child: FlatButton(
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                new BorderRadius.circular(18.0),
+                                            side: BorderSide(
+                                                color: Color(0xffb4194b)),
+                                          ),
+                                          onPressed: () {},
+                                          child: Text('Guest'),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Container(
+                                    alignment: Alignment.centerLeft,
+                                    padding:
+                                        EdgeInsets.only(top: 10.0, bottom: 30),
+                                    child: Wrap(children: <Widget>[
+                                      Text(
+                                        'Use your username and password',
+                                        textScaleFactor: 1,
+                                      ),
+                                    ]),
+                                  ),
+                                  TextFormField(
+                                      decoration: InputDecoration(
+                                        labelText: 'Email',
+                                      ),
+                                      validator: (value) {
+                                        if (value.isEmpty) {
+                                          return 'Please enter your first name';
+                                        }
+                                      },
+                                      onSaved: (val) => null
+                                      // setState(() => _user.firstName = val),
+                                      ),
+                                  TextFormField(
+                                    decoration: InputDecoration(
+                                      labelText: 'Password',
+                                      suffixIcon: GestureDetector(
+                                        onTap: () {
+                                          // Update the state i.e. toogle the state of passwordVisible variable
+                                          setState(() {
+                                            passwordVisible = !passwordVisible;
+                                            _obsecureText = !_obsecureText;
+                                          });
+                                        },
+                                        child: Icon(
+                                          // Based on passwordVisible state choose the icon
+                                          passwordVisible
+                                              ? Icons.visibility
+                                              : Icons.visibility_off,
+                                          color: Theme.of(context)
+                                              .primaryColorDark,
+                                        ),
+                                      ),
+                                    ),
+                                    validator: (value) {
+                                      if (value.isEmpty) {
+                                        return 'Please enter your first name';
+                                      }
+                                    },
+                                    onSaved: (val) => null,
+                                    obscureText: _obsecureText,
+                                    // setState(() => _user.firstName = val),
+                                  ),
+                                  Wrap(children: <Widget>[
+                                    Container(
+                                      alignment: Alignment.centerRight,
+                                      child: FlatButton(
+                                        onPressed: () {},
+                                        child: Text(
+                                          'Forgot Password',
+                                        ),
+                                      ),
+                                    ),
+                                  ]),
+                                  Row(
+                                    children: <Widget>[
+                                      Expanded(
+                                        child: FlatButton(
+                                          child: Text('Login'),
+                                          textColor: Colors.white,
+                                          color: Color(0xffb4194b),
+                                          shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(30.0)),
+                                          onPressed: () {},
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text('Dont have an account?'),
+                                      FlatButton(
+                                        child: Text('Sign up'),
+                                        textColor: Colors.blue,
+                                        onPressed: () {},
+                                      )
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
                     )
-                  ],
-                )
-              
-            
+                    // RaisedButton(
+                    //   child: Text("Login with googe"),
+                    //   onPressed: () {
+                    //     _handleSignIn();
+                    //   },
+                    // ),
+                    )
+              ],
+            )
           : Container(
               child: Column(
                 children: <Widget>[
